b145316d66dd30a31f3fcd1e1505a85a36e5e489
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-09-09 21:31:20 -0300

    appointments model

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,18 +1,25 @@
-import { Router, request, response } from 'express';
-import { uuid } from 'uuidv4';
+import { Router } from 'express';
+import { parseISO, startOfHour, isEqual } from 'date-fns';
+import Appointment from '../models/Appointments';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+const appointments: Appointment[] = [];
 
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
   
-  const appointment = {
-    id: uuid(),
-    provider,
-    date
-  };
+  const findAppoitmentInSameDate = appointments.find(appointment => 
+    isEqual(appointment.date, parsedDate)
+  );
+
+  if (findAppoitmentInSameDate){
+    return response.status(400).json({message: "This hour is already booked."});  
+  } 
+
+  const appointment = new Appointment(provider, parsedDate);
 
   appointments.push(appointment);
   
