44eea01a47a70cd67939954799c568c8824cd786
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2020-09-15 11:24:09 -0300

    alterando o model de agendamentos

--- src/models/Appointments.ts
+++ src/models/Appointments.ts
@@ -1,15 +1,26 @@
-import { uuid } from "uuidv4";
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+/**
+ *  Declarando um decorator.
+ * Quando declaramos um decorator em cima de uma classe significa que estamos 
+ * passando ela, a classe, como um parametro para a nossa entidade.
+ * Decorators estão disponíveis apenas no typescript, não no javascript.
+ * 
+ * Quando utilizamos o typeorm e os decorators não é necessário mais utilizar o 
+ * método constructor.
+ * a bib uuid também não precisa mais ser importada porque o typeorm já faz isso.
+ */
+
+ @Entity('appointments')
 class Appointment {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-  provider: string;
-  date: Date;
 
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
+  @Column()
+  provider: string;
+  
+  @Column('timestamp with time zone')
+  date: Date;
 }
 
 export default Appointment;
