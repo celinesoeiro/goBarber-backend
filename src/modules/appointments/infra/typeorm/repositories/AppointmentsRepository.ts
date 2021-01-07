633df7c51d8061d25575365c2d14f7cfd632f855
Celine Soeiro <52112166+celinesoeiro@users.noreply.github.com>
2021-01-07 15:11:01 -0300

    liskov substitution principle

--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,23 +1,24 @@
 import { EntityRepository, Repository } from 'typeorm';
 
 import Appointment from "@modules/appointments/infra/typeorm/entities/Appointments";
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
+class AppointmentsRepository extends Repository<Appointment> implements IAppointmentsRepository {
 
-  public async findByDate(date: Date): Promise<Appointment | null> {
+  public async findByDate(date: Date): Promise<Appointment | undefined> {
     const findAppoitment = await this.findOne({
       where: { date },
     })
 
-    return findAppoitment || null;
+    return findAppoitment;
   }
 }
 
 export default AppointmentsRepository;
 
-/** 
- * Quando se usa async/await tranforma a classe em uma promise. 
+/**
+ * Quando se usa async/await tranforma a classe em uma promise.
  * Logo, a resposta pode ser obtida assim:
  * findByDate(date).then( res => {} ).catch( err => {} )
  * onde res é do tipo Appointment ou null (nesse caso)
